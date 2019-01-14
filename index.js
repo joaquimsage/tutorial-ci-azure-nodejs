ed79085b05b413dd7006d046bdc23ee8642e4149
Quino <joaquim.forcada@sage.com>
2019-01-14 12:46:12 +0100

    Develop Publish node version

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var http = require('http');
 
 var server = http.createServer(function(request, response) {
 
+    let nodeVersion = process.version;
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World, Quino nodejs web sample app Develop branch has been merged.!");
+    response.end("Hello World, Quino nodejs web sample app Develop branch has been merged.!" + " Nodejs version = " + nodeVersion);
 
 });
 
