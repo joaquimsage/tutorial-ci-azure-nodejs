881ccf54bb07ae1cff759733f5aeb08236b74f91
Quino <joaquim.forcada@sage.com>
2019-01-14 12:35:14 +0100

    Develop commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World, Quino nodejs web sample app!");
+    response.end("Hello World, Quino nodejs web sample app Develop branch has been merged.!");
 
 });
 
