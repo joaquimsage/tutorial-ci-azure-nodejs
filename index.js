6effbc60634dc86a4c149fd41f2263af86037a3a
Quino <joaquim.forcada@sage.com>
2019-01-14 17:28:50 +0100

    Remvoe process node version

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function(request, response) {
 
-    let nodeVersion = process.version;
+    let nodeVersion = ' Yo que se';
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World, Quino nodejs web sample app Develop branch has been merged.!" + " Nodejs version = " + nodeVersion);
 
