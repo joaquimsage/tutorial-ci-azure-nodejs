44aee68ea6a386c9b817cdc05f719427da1b7e4b
CI Auto-process <joaquim.forcada@sage.com>
2019-01-15 11:13:00 +0100

    1.9.0-RC.0

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var server = http.createServer(function(request, response) {
 
     let nodeVersion = process.version;
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World, Quino nodejs web sample app Develop branch has been merged. deployed with auto Azure DevOps" + " Nodejs version = " + nodeVersion);
+    response.end("Hello World, Quino nodejs web sample app Develop branch has been merged. deployed 1.9.0-RC.0" + " Nodejs version = " + nodeVersion);
 
 });
 
