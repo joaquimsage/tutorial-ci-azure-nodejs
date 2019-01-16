868ff2fc761b2096c7a22d3599cd697d11eb16ef
CI Auto-process <joaquim.forcada@sage.com>
2019-01-16 12:33:44 +0100

    PR script

--- scripts/ci/release/production/githubCreatePRToMaster.sh
+++ scripts/ci/release/production/githubCreatePRToMaster.sh
@@ -31,7 +31,9 @@ pull_request() {
   from_user=$(echo $origin | sed -e 's/.*[\/:]\([^/]*\)\/[^/]*$/\1/')
   repo=$(basename `git rev-parse --show-toplevel`)
   from_branch=$(git rev-parse --abbrev-ref HEAD)
-  open "https://github.com/$to_user/$repo/pull/new/$to_user:$to_branch...$from_user:$from_branch"
+
+  echo https://github.com/$to_user/$repo/pull/new/$to_user:$to_branch...$from_user:$from_branch
+  # open "https://github.com/$to_user/$repo/pull/new/$to_user:$to_branch...$from_user:$from_branch"
 }
 
 # usage
